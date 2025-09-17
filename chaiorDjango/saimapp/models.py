8942ab9bc12ee2c59277d6cf57aa13f44a3f49f1
saim <saimfaisal.official.com@gmail.com>
2025-09-17 00:53:14 -0700

    add models

--- chaiorDjango/saimapp/models.py
+++ chaiorDjango/saimapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class ChaiVarity(models.Model):
+   CHAI_TYPR_CHOICE =[
+      ('ML','MASALA')
+      ('GR','GINJAR')
+      ('KI','KIWI')
+     ]
+   name = models.CharField(max_length=100)
+   image = models.models.ImageField(upload_to='chais/')
+   date_added = models.DateTimeField(default=timezone.now)
